014036d8337792269b58cd3e941f0865249177a8
Mohamed khaloti <simooo@Mohameds-MacBook-Pro.local>
2019-04-08 12:58:41 -0400

    MVP Completed

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,30 +1,30 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 // Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const HeaderContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-margin-top: 40px;
-width: 100%;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 40px;
+  width: 100%;
+`;
 const Date = styled.span`
-margin-left: 25px;
-`
+  margin-left: 25px;
+`;
 const MainHeading = styled.h1`
-font-size: 60px;
+  font-size: 60px;
   font-family: Didot, serif;
   font-weight: bold;
   flex: 8;
   text-align: center;
   color: #000;
-`
+`;
 const Temp = styled.span`
-margin-right: 25px;
-`
+  margin-right: 25px;
+`;
 
 const Header = () => {
   return (
